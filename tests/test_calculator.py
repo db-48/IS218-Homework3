67be2876c2114f25b7664c3e4938de875f08436b
db-48 <db48@njit.edu>
2024-09-27 09:13:04 -0400

    added multiplication and divison to test_calculator

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -6,6 +6,13 @@ def test_addition():
     assert add(2,2) == 4
 
 def test_subtraction():
-    '''Test that addition function works '''    
+    '''Test that subtraction function works '''    
     assert subtract(2,2) == 0
-    
+
+def test_multiply():
+    '''Test that multiplication function works '''    
+    assert Calculator.multiply(2,2) == 4
+
+def test_divide():
+    '''Test that division function works '''    
+    assert Calculator.divide(2,2) == 1 
