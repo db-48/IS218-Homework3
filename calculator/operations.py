573c32e4cf68fe568406b1f65716182fcbd77af1
db-48 <db48@njit.edu>
2024-10-01 19:08:16 -0400

    updated def multiply

--- calculator/operations.py
+++ calculator/operations.py
@@ -6,7 +6,7 @@ def add(a: Decimal, b: Decimal) -> Decimal:
 def subtract(a: Decimal, b: Decimal) -> Decimal:
     return a - b
 
-def multiply(a,b):
+def multiply(a: Decimal, b: Decimal) -> Decimal:
     return a * b
 
 def divide(a,b):
