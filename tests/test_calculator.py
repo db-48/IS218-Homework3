7e5e7cd9203f21f7af5f644aecf90aa6437678c7
db-48 <db48@njit.edu>
2024-10-01 18:06:25 -0400

    updated test_calculator

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,13 +1,13 @@
 '''My Calculator Test'''
-from calculator import add, subtract
+from calculator import Calculator
 
 def test_addition():
     '''Test that addition function works '''    
-    assert add(2,2) == 4
+    assert Calculator.add(2,2) == 4
 
 def test_subtraction():
     '''Test that subtraction function works '''    
-    assert subtract(2,2) == 0
+    assert Calculator.subtract(2,2) == 0
 
 def test_multiply():
     '''Test that multiplication function works '''    
