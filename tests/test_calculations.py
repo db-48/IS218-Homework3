23714cbbfffc46259a6f724fcb17fbdb54d0f093
db-48 <db48@njit.edu>
2024-10-01 18:32:46 -0400

    added imports to test_calculations

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,19 +1,12 @@
 '''My Calculator Test'''
-from calculator.operations import add, multiply, subtract, divide
 
-def test_addition():
-    '''Test that addition function works '''    
-    assert add(2,2) == 4
+from decimal import Decimal
+import pytest
 
-def test_subtraction():
-    '''Test that addition function works '''    
-    assert subtract(2,2) == 0
+from calculator.calculation import Calculation
+from calculator.calculations import Calculations
 
-def test_multiplication():
-    '''Test that multiply function works'''
-    assert multiply(2,2) == 4
+from calculator.operations import add, subtract
 
-def test_division():
-    '''Test that division function works'''
-    assert divide(2,2) == 1
+
     
