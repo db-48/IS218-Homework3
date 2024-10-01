0fc23e2cc55c952f2c0d96750e157aa0d0ad3699
db-48 <db48@njit.edu>
2024-10-01 19:31:06 -0400

    added add_calculation

--- calculator/calculations.py
+++ calculator/calculations.py
@@ -6,4 +6,6 @@ from calculator.calculation import Calculation
 class Calculations:
     history: List[Calculation] = []
 
-    
+    @classmethod
+    def add_calculation(cls, calculation: Calculation):
+        cls.history.append(calculation)
