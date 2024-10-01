708e5ce8ac37828f35a8ca7d7d67096771339c44
db-48 <db48@njit.edu>
2024-10-01 19:15:29 -0400

    updated def add

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -11,18 +11,21 @@ class Calculator:
         calculation = Calculation.create(a, b, operation)
         Calculations.add_calculation(calculation)
         return calculation.perform()
+    
     @staticmethod
-    def add(a,b):
-        calculation = Calculation(a, b, add)  # Pass the add function from calculator.operations
-        return calculation.get_result()
+    def add(a: Decimal, b: Decimal) -> Decimal:
+        return Calculator._perform_operation(a, b, add)
+    
     @staticmethod
     def subtract(a,b):
         calculation = Calculation(a, b, subtract)  # Pass the subtract function from calculator.operations
         return calculation.get_result()
+    
     @staticmethod
     def multiply (a,b):
         calculation = Calculation(a, b, multiply)  # Pass the multiply function from calculator.operations
         return calculation.get_result()
+    
     @staticmethod
     def divide(a,b):
         calculation = Calculation(a, b, divide)  # Pass the divide function from calculator.operations
