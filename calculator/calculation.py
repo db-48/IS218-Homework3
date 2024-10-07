27dfdc85ecbcaa074fccbdd61bc8d477289048eb
db-48 <db48@njit.edu>
2024-10-07 18:31:48 -0400

    updated calcualton .py

--- calculator/calculation.py
+++ calculator/calculation.py
@@ -1,6 +1,6 @@
 from decimal import Decimal
 from typing import Callable
-from calculator.operations import add, subtract, multiply, divide
+from .operations import add, subtract, multiply, divide
 
 class Calculation:
     def __init__(self, a: Decimal, b: Decimal, operation: Callable[[Decimal, Decimal], Decimal]):
@@ -11,7 +11,6 @@ class Calculation:
 @staticmethod    
 def create(a: Decimal, b: Decimal, operation: Callable[[Decimal, Decimal], Decimal]):
         return Calculation(a, b, operation)
-
 
 def perform(self) -> Decimal:
         return self.operation(self.a, self.b)
